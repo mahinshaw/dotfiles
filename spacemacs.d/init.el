cef183ce87641c6110d3f9a0a678728a2a52a007
Mark Hinshaw <mahinshaw@gmail.com>
2018-02-23 08:42:18 -0800

    Cleanup spacemaces init.

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -72,7 +72,7 @@ This function should only modify configuration layer settings."
 
      ;; LANGUAGES
      ;; asciidoc
-     ;; csharp
+     csharp
      clojure
      emacs-lisp
      ;; erlang
@@ -86,11 +86,13 @@ This function should only modify configuration layer settings."
      html
      ;; idris
      (java :variables java-backend 'meghanada)
-     ;; lsp
+     lsp
      ;; lsp-java
      ;; javascript
      kotlin
-     markdown
+     (markdown :variables
+               markdown-command "cmark-gfm -e table"
+               markdown-live-preview-engine 'vmd)
      ocaml
      ;; reasonml
      ;; php
@@ -123,9 +125,6 @@ This function should only modify configuration layer settings."
                                       apache-mode
                                       direnv
                                       inf-clojure
-                                      ;; lsp-mode
-                                      ;; (lsp-ui :location (recipe :fetcher github :repo "emacs-lsp/lsp-ui"))
-                                      ;; lsp-java
                                       vue-mode
                                       vue-html-mode
                                       ;; sicp
@@ -166,9 +165,15 @@ It should only modify the values of Spacemacs settings."
    ;; (default 5)
    dotspacemacs-elpa-timeout 5
 
+   ;; Set `gc-cons-threshold' and `gc-cons-percentage' when startup finishes.
+   ;; This is an advanced option and should not be changed unless you suspect
+   ;; performance issues due to garbage collection operations.
+   ;; (default '(100000000 0.1))
+   dotspacemacs-gc-cons '(100000000 0.1)
+
    ;; If non-nil then Spacelpa repository is the primary source to install
-   ;; a locked version of packages. If nil then Spacemacs will install the lastest
-   ;; version of packages from MELPA. (default nil)
+   ;; a locked version of packages. If nil then Spacemacs will install the
+   ;; latest version of packages from MELPA. (default nil)
    dotspacemacs-use-spacelpa nil
 
    ;; If non-nil then verify the signature for downloaded Spacelpa archives.
@@ -459,11 +464,6 @@ It should only modify the values of Spacemacs settings."
    ;; (default '("rg" "ag" "pt" "ack" "grep"))
    dotspacemacs-search-tools '("rg" "ag" "pt" "ack" "grep")
 
-   ;; The default package repository used if no explicit repository has been
-   ;; specified with an installed package.
-   ;; Not used for now. (default nil)
-   dotspacemacs-default-package-repository nil
-
    ;; Format specification for setting the frame title.
    ;; %a - the `abbreviated-file-name', or `buffer-name'
    ;; %t - `projectile-project-name'
@@ -510,6 +510,10 @@ configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
 
+  ;; (setq configuration-layer-elpa-archives '(("melpa" . "melpa.org/packages/")
+  ;;                                           ("org" . "orgmode.org/elpa/")
+  ;;                                           ("gnu" . "elpa.gnu.org/packages/")))
+
   (global-set-key (kbd "C-s") 'isearch-forward-regexp)
   (global-set-key (kbd "C-r") 'isearch-backward-regexp)
   (global-set-key (kbd "C-M-s") 'isearch-forward)
@@ -547,6 +551,9 @@ before packages are loaded."
   ;; persistence with desktop mode.
   ;; (desktop-save-mode t)
   ;; (desktop-read)
+
+  ;; java 2 spaces please
+  (add-hook 'java-mode-hook (lambda () (setq c-basic-offset 2)))
 
   ;; projectile hooks
   (with-eval-after-load 'projectile
