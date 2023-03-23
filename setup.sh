274065aea812d0564a80ef6092d3d00d842caaa1
Mark Hinshaw <mhinshaw@kollective.com>
2023-03-23 13:22:42 -0700

    Update brewfile; add setup.sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#install homebrew
+eval "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# # pull git repo for dotfiles
+# git clone https://github.com/mahinshaw/dotfiles ~/.dotfiles
+
+# cd ~/.dotfiles
+
+#install brew bundle Brewfile
+brew bundle
+
+# install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# use zsh
+chsh -s $(which zsh)
+
+# use prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
