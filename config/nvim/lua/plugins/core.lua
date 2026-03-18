2300b840e9a851843b63c548e0e9bd5be92defa4
Mark Hinshaw <mhinshaw@kollective.com>
2026-03-18 13:40:05 -0700

    Update snacks to search hidden files; Update deps.

--- config/nvim/lua/plugins/core.lua
+++ config/nvim/lua/plugins/core.lua
@@ -1,6 +1,17 @@
 return {
   { "folke/lazy.nvim", version = false },
   {
+    "folke/snacks.nvim",
+    version = false,
+    opts = {
+      picker = {
+        hidden = true,
+        ignore = true,
+        sources = { files = { show_unindexed = true, hidden = true, ignore = true } },
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     version = false,
     opts = {
